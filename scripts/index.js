60d64516af097a9ae740c75266f98b4311ab5a97
martins-th <thiago.martins20x@gmail.com>
2025-12-21 00:48:17 -0300

    botoes-header

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const botao1 = document.getElementById ('btn_cascata1');
+const menu1 = document.getElementById ('menu_lores');
+
+const botao2 = document.getElementById ('btn_cascata2');
+const menu2 = document.getElementById ('menu_fontes');
+
+botao1.addEventListener ('click', function() {
+    menu1.classList.toggle('aberto')
+});
+
+botao2.addEventListener ('click', function() {
+    menu2.classList.toggle('aberto')
+})
+
+window.onclick = function(event) {
+    if(!event.target.matches('#btn_cascata1')) {
+        if(menu1.classList.contains('aberto')) {
+            menu1.classList.remove('aberto')
+        }
+    }
+
+    if(!event.target.matches('#btn_cascata2')) {
+        if(menu2.classList.contains('aberto')) {
+            menu2.classList.remove('aberto')
+        }
+    }
+}
+
+
+
