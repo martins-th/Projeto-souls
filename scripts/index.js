6dd206f833b88822249fe976d92b0c9bb761f7e0
martins-th <thiago.martins20x@gmail.com>
2026-01-04 15:41:46 -0300

    separacao-footer-correcao-botoes-header

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,60 @@
-console.log(document.querySelector('.botao1'));
+
 function loadComponent(id, path) {
-    fetch(path) 
+    return fetch(path) 
         .then(resposta => resposta.text())
         .then(dados => {
-            document.getElementById(id).innerHTML = dados
+            document.getElementById(id).innerHTML = dados;
         })
         .catch(erro => console.error('Erro ao carregar componente', erro));
+};
+
+function carregaMenu() {
+    const botao1=document.getElementById('btn_cascata1');
+    const botao2=document.getElementById('btn_cascata2');
+
+    if(botao1){
+        botao1.addEventListener('click', () => {
+            document.querySelector('.botao1_lista').classList.toggle('aberto');
+        });
+    }
+    
+    if(botao2){
+        botao2.addEventListener('click', ()=> {
+            document.querySelector('.botao2_lista').classList.toggle('aberto');
+        });
+    }
 }
 
-loadComponent ('header_placeholder', '/paginas/header.html');
-
-
-
-const botao1 = document.getElementById ('btn_cascata1');
-const menu1 = document.getElementById ('menu_lores');
-
-const botao2 = document.getElementById ('btn_cascata2');
-const menu2 = document.getElementById ('menu_fontes');
-
-botao1.addEventListener ('click', function() {
-    menu1.classList.toggle('aberto')
+loadComponent ('header_placeholder', '/paginas/header.html').then( () => {
+    carregaMenu();
 });
 
-botao2.addEventListener ('click', function() {
-    menu2.classList.toggle('aberto')
+loadComponent ('footer_placeholder', '/paginas/footer.html');
+
+document.addEventListener('click', (event) => {
+    const btn1=document.getElementById('btn_cascata1');
+    const lista1=document.querySelector('.botao1_lista');
+
+    const btn2=document.getElementById('btn_cascata2');
+    const lista2=document.querySelector('.botao2_lista');
+
+    if (btn1 && lista1.classList.contains('aberto')) {
+        const clicouNoBotao1 = btn1.contains(event.target);
+        const clicouNoMenu1 = lista1.contains(event.target);
+
+        if(!clicouNoBotao1 && !clicouNoMenu1) {
+            lista1.classList.remove('aberto');
+        }
+    }
+
+    if (btn2 && lista2.classList.contains('aberto')) {
+        const clicouNoBotao2 = btn2.contains(event.target);
+        const clicouNoMenu2 = lista2.contains(event.target);
+
+        if(!clicouNoBotao2 && !clicouNoMenu2) {
+            lista2.classList.remove('aberto');
+        }
+    }
 })
 
-window.onclick = function(event) {
-    if(!event.target.matches('#btn_cascata1')) {
-        if(menu1.classList.contains('aberto')) {
-            menu1.classList.remove('aberto')
-        }
-    }
-
-    if(!event.target.matches('#btn_cascata2')) {
-        if(menu2.classList.contains('aberto')) {
-            menu2.classList.remove('aberto')
-        }
-    }
-}
-
-
-
+console.log("quem leu é gay");
