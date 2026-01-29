886fd230e81d00820aaf6e7cc58c0dae18277dff
martins-th <thiago.martins20x@gmail.com>
2026-01-29 14:39:44 -0300

    pagina-curiosidades-e-fim

--- scripts/curiosidades.js
+++ scripts/curiosidades.js
@@ -1,25 +1,17 @@
-async function carregaIceberg () {
+async function carregaTudo () {
     try {
         const resposta = await fetch("../curiosidades.json");
         const dados = await resposta.json();
 
-        const lista = document.getElementById("iceberg");
+        const urlTextoCriador = dados.criador[0].texto;
+        const buscaTexto = await fetch(urlTextoCriador);
+        const textoFinal = await buscaTexto.text();
 
-        let itensListaAcumulado = "";
-
-        for(let i=0; i< dados.length; i ++ ) {
-            if (i % 4 === 0){
-                let numeroCamada = (i/4) + 1;
-                itensListaAcumulado += `<li class="camada_titulo">Camada ${numeroCamada}</li>`;
-            };
-
-            itensListaAcumulado += `<li class="camada_item">
-                        <img src="${dados[i].imagem}">
-                        <p>${dados[i].texto}</p>
-                        </li>`;
-        }
-
-        lista.innerHTML = itensListaAcumulado;
+        renderizaIceberg(dados.iceberg);
+        renderizaEaster(dados.easterEggs);
+        renderizaFilosofia(dados.filosofia);
+        renderizaCriador(textoFinal);
+        //passando os arrays de objeto como argumento
 
     }
     catch (erro) {
@@ -27,4 +19,62 @@ async function carregaIceberg () {
     }
 }
 
-carregaIceberg();
+function renderizaIceberg (icebergDados) {
+    const lista = document.getElementById("iceberg");
+    if(!lista) return;
+
+    let itensListaAcumulado = "";
+
+    for(let i=0; i< icebergDados.length; i ++ ) {
+        if (i % 4 === 0){
+            let numeroCamada = (i/4) + 1;
+            itensListaAcumulado += `<li class="camada_titulo">Camada ${numeroCamada}</li>`;
+        };
+
+        itensListaAcumulado += `<li class="camada_item">
+                    <img src="${icebergDados[i].imagem}">
+                    <p>${icebergDados[i].texto}</p>
+                    </li>`;
+    }
+
+    lista.innerHTML = itensListaAcumulado;
+};
+//aqui a função recebe icebergDados como parâmetro. Pelo uso do for perceb-se que o parametro é um array e vai ser utilizado ao longo de
+//toda a função. Qualquer função vai rodar somente quando eu chamar a carrega tudo 
+//(ela vai ser um inicializador de funções com parametros escolhidos)
+
+function renderizaEaster (easterDados) {
+
+    const listaEaster = document.getElementById("easter");
+    if(!listaEaster) return;
+
+    let itensEasterAcumulado = "";
+
+    for(let i=0; i< easterDados.length; i ++){
+        itensEasterAcumulado += `<li class="camada_item">
+        <p>${easterDados[i].texto}</p></li>`;
+    }
+
+    listaEaster.innerHTML = itensEasterAcumulado;
+}
+
+function renderizaFilosofia (filosofiaDados) {
+    const listaFilosofia = document.getElementById("filosofia");
+    if(!listaFilosofia) return;
+
+    let itensFilosofiaAcumulado = "";
+
+    for(i=0; i< filosofiaDados.length; i++){
+        itensFilosofiaAcumulado += `<li class="camada_filosofia">
+            <p>${filosofiaDados[i].titulo}</p>
+            <p>${filosofiaDados[i].texto}</p></li>`;
+    }
+
+    listaFilosofia.innerHTML = itensFilosofiaAcumulado;
+}
+
+function renderizaCriador (criadorDados) {
+    document.querySelector("#biografia").textContent = criadorDados;
+}
+
+carregaTudo();
