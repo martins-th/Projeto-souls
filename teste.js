35d3ff93b239ffeccd1272020fa4958904c49677
Thiago Martins <159711059+martins-th@users.noreply.github.com>
2026-04-19 16:57:25 -0300

    criação-curiosidades

--- teste.js
+++ teste.js
@@ -78,6 +78,58 @@ app.get('/personagens/:nome', async (req, res) => {
     }
 })
 
+/*app.get('/curiosidades', async (req, res) => {
+    try {
+        //esse é o mesmo tipo de consulta que fizemos no caso dos personagens (caso seja um sistema que cresça, teremos que mudar em vários lugares)
+        //sei que não irá crescer, mas temos outras formas de fazer isso.
+        const [[rows_camada1], [rows_camada2], [rows_camada3], [rows_camada4], [rows_camada5], [rows_camada6]] = await Promise.all([
+            db.execute('SELECT * FROM curiosidades WHERE camada = 1'),
+            db.execute('SELECT * FROM curiosidades WHERE camada = 2'),
+            db.execute('SELECT * FROM curiosidades WHERE camada = 3'),
+            db.execute('SELECT * FROM curiosidades WHERE camada = 4'),
+            db.execute('SELECT * FROM curiosidades WHERE camada = 5'),
+            db.execute('SELECT * FROM curiosidades WHERE camada = 6')
+        ]);
+
+        res.render('curiosidades', { curiosidades_c1: rows_camada1, curiosidades_c2: rows_camada2, curiosidades_c3: rows_camada3, curiosidades_c4: rows_camada4, curiosidades_c5: rows_camada5, curiosidades_c6: rows_camada6 });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("erro ao carregar camadas");
+    }
+})
+*/
+
+//fazendo de outro modo
+//deixei a rota diferente pra não dar conflito, se der certo eu mantenho esse e comento o outro.
+app.get ('/curiosidades', async (req, res) =>{
+    try {
+        //farei uma única consulta ao banco
+        const [todasAsLinhas] = await db.execute('SELECT * FROM curiosidades ORDER BY camada ASC');
+        //agora criamos um objeto/array vazio para definir as camadas
+        const camadas = {};
+        //criamos a lógica de iteração para cada camada
+        //forEach passa por cada posição do array fazendo alguma coisa. Nesse caso, ele chama cada posição de item e verifica se existe um array com o valor daquela camada
+        //nessa consulta ele cria arrays para cada camada.
+        todasAsLinhas.forEach(item => {
+            if(!camadas[item.camada]) {      //aqui ele passa pela coluna camada de cada item dos dados que vieram do banco e verifica se já existe um array vazio com aquela numeração de camada
+                camadas[item.camada] = []; //cria o array para a camada
+            }
+            camadas[item.camada].push(item); //se o item analizado tem a mesma camada do item.camada, então o item (linha do db) vai ser colocado dentro do array dessa camada;
+        });
+
+        //agora tentando fazer rodar sem nenhuma dúvida com a IA
+        //por estar usando o hbs, tenho que entregar tudo renderizado especificando a página a ser carregada e as relações que cada item vai ter, então:
+        //posso renderizar grupo a grupo especificando laços para cada section que eu fizer no html:
+        //res.render('curiosidades', {curiosidades_c1: camadas[1], curiosidades_c2: camadas[2], curiosidades_c3: camadas[3], curiosidades_c4: camadas[4], curiosidades_c5: camadas[5], curiosidades_c6: camadas[6]});
+        //ou renderizar tudo de uma só vez e conseguir escrever somente uma section no html deixando menos poluido ainda
+        //já que tudo tá armazenado na const camadas, preciso somente renderizar isso e depois criar a estrutura html pra poder repetir
+        res.render('curiosidades', {camadas});
+    } catch (error){
+        console.error(error);
+        res.status(500).send("erro ao carregar camadas");
+    }
+})
+
 app.listen(8081, function () {
     console.log("Server rodando!");
 })
